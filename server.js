a3adbf3ebe62e5606505d49336fdb8782c6aae57
Bryan Burnside <bpburnside@gmail.com>
2023-06-11 13:30:30 -0500

    Create simple video-conferencing-app

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app); // Create server for socket.io
+const io = require('socket.io')(server);
+const { v4: generateRoomName } = require('uuid'); // Use UUID's V4 function to generate unique room name
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${generateRoomName()}`);
+});
+
+app.get('/:room', (req, res) => {
+  const { roomId } = req.params
+  res.render('room', { roomId });
+});
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
